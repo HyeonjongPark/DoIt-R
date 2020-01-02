d34817d62714e86ce6073b933798c783c23f2f13
HyeonjongPark <guswhd9844@gmail.com>
2020-01-02 11:17:25 +0900

    연령대별 임금 추가

--- 1_Korean-life-style/3_age-and-income.R
+++ 1_Korean-life-style/3_age-and-income.R
@@ -1,3 +1,5 @@
+### 나이와 월급의 관계
+
 class(welfare$birth)
 summary(welfare$birth)
 
@@ -21,6 +23,35 @@ age_income
 
 # 나이에 따른 월급 평균 그래프
 ggplot(data = age_income , aes(x = age, y=mean_income)) + geom_line()
+
+
+
+
+
+### 연령대에 따른 월급 차이
+
+welfare = welfare %>% 
+  mutate(ageg = ifelse(age < 30, "young", 
+                       ifelse(age <= 59, "middle", "old")))
+
+table(welfare$ageg)
+qplot(welfare$ageg)
+
+ageg_income = welfare %>% 
+  filter(!is.na(income)) %>% 
+  group_by(ageg) %>% 
+  summarise(mean_income = mean(income))
+
+ageg_income
+
+ggplot(ageg_income , aes(x = ageg, y = mean_income)) + geom_col()
+
+# 정렬 다시
+ggplot(ageg_income , aes(x = ageg, y = mean_income)) +
+  geom_col() +
+  scale_x_discrete(limits = c("young", "middle", "old"))
+
+
 
 
 
